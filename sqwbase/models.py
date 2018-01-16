4f4fcdfad12551977ae7e2a76f2c07cb84413698
Johannes Steinmetzer <jsteinmetzer1108@aol.com>
2018-01-16 10:54:29 +0100

    added description field to Molecule and added it to some templates

--- sqwbase/models.py
+++ sqwbase/models.py
@@ -34,6 +34,7 @@ class Molecule(models.Model):
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
     workflow = models.ManyToManyField(Workflow)
+    description = models.TextField(blank=True)
 
     def __str__(self):
         return self.name
