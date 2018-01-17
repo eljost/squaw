1f2ca7a463dd776601c85a62f844ca69b5aa46c3
Johannes Steinmetzer <jsteinmetzer1108@aol.com>
2018-01-17 08:05:55 +0100

    expanded pdb saving

--- sqwbase/models.py
+++ sqwbase/models.py
@@ -2,6 +2,18 @@ from django.db import models
 from mptt.models import MPTTModel, TreeForeignKey
 
 
+
+def rename_pdb(instance, filename):
+    if isinstance(instance, Molecule):
+        prefix = "molecule_"
+    elif isinstance(instance, Calculation):
+        prefix = "calc_"
+    else:
+        prefix = ""
+    save_to = "pdbs/" + f"{prefix}pk{instance.pk}_" + filename
+    return save_to
+
+
 class Project(models.Model):
     name = models.CharField(max_length=100)
 
@@ -35,6 +47,7 @@ class Molecule(models.Model):
     name = models.CharField(max_length=200)
     workflow = models.ManyToManyField(Workflow)
     description = models.TextField(blank=True)
+    pdb_file = models.FileField(blank=True, upload_to=rename_pdb)
 
     def __str__(self):
         return self.name
@@ -85,7 +98,7 @@ class Calculation(models.Model):
                               on_delete=models.CASCADE)
     program = models.ForeignKey(Program, null=True, blank=True,
                                 on_delete=models.CASCADE)
-    pdb_file = models.FileField(blank=True, upload_to="pdbs/")
+    pdb_file = models.FileField(blank=True, upload_to=rename_pdb)
 
     def __str__(self):
         return self.title
