ae7d0dc58dd600aace5612ced5fa4a798fd5eec8
Johannes Steinmetzer <jsteinmetzer1108@aol.com>
2018-01-17 08:33:30 +0100

    made calcsyncer dependency optional

--- sqwbase/apps.py
+++ sqwbase/apps.py
@@ -1,12 +1,19 @@
+import logging
+
 from django.apps import AppConfig
 from django.db.models.signals import post_save
-
-from sqwbase.signals import calculation_callback
 
 
 class SqwbaseConfig(AppConfig):
     name = 'sqwbase'
 
     def ready(self):
+        try:
+            from calcsyncer.main import sync_calculation
+        except:
+           logging.warning("Couldn't find calcsycner. Skipping registration "
+                           "of post_save receiver for calculations.")
+           return
+        from sqwbase.signals import calculation_callback
         from sqwbase.models import Calculation
         post_save.connect(calculation_callback, sender=Calculation)
